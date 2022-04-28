4499f8c484ef45a2b3de76f8cd8bfe3b60cf0394
KyngCoder <keshawnrkmerchant9@gmail.com>
2022-04-28 11:27:53 -0500

    updated main

--- src/Main.js
+++ src/Main.js
@@ -20,15 +20,10 @@ function Main({ searchTerm, category }) {
         )
         .then((response) => setAnimeList(response.data));
     } else {
-      const animes = await axios
-        .get(
-          `https://animechan.vercel.app/api/quotes/anime?title=${searchTerm}`
-        )
+      const animes = await axios.get(`https://animechan.vercel.app/api/quotes/anime?title=${searchTerm}`)
         .then((response) => setAnimeList(response.data));
     }
   };
-
- 
 
 
 
@@ -36,36 +31,6 @@ function Main({ searchTerm, category }) {
     getAnime();
   }, [searchTerm]);
 
-
-import React from 'react'
-import {v4} from 'uuid'
-import {useEffect,useState} from 'react'
-import Card from './Card'
-import axios from 'axios'
-
-function Main({searchTerm,category}) {
-
-    const [animeList,setAnimeList] = useState([])
-
-    const getAnime = async () => {
-      
-            if(category === 'random'){
-            const animes = await axios.get(`https://animechan.vercel.app/api/quotes`) 
-            .then(response => setAnimeList(response.data))
-            }else if(category === 'character'){
-                const animes = await axios.get(`https://animechan.vercel.app/api/quotes/character?name=${searchTerm}`) 
-                .then(response => setAnimeList(response.data))
-            }else {
-                const animes = await axios.get(`https://animechan.vercel.app/api/quotes/anime?title=${searchTerm}`) .then(response => setAnimeList(response.data))
-                
-            }
-        
-       
-      }
-    
-      useEffect(()=>{
-       getAnime()
-      },[searchTerm]) 
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 m-6">
       {animeList.map(anime => {
